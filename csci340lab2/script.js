2d0d89323a56f46a69e96b960bad2ad0dbcac3b6
Alivia-Allen <AllenAA@Hendrix.edu>
2021-09-13 22:27:30 -0500

    Update script.js

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -2,14 +2,8 @@ $(document).ready(function() {
   $('.cookie').click(function() {
     $.ajax({
       dataType: "jsonp",
-      jsonpCallback: "parseQuote",
-      url: "https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=parseQuote",
-      success: function(results) {
-        $('.fortune').text(results["quoteText"]);
-      },
-      error: function(xhr,status,error) {
-        console.log(error);
-      }
+     jsonpCallback: "parseQuote",
+     url: "https://api.adviceslip.com/advice/jsonpCallback=[advice]"
     });
   });
 });
